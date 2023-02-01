07da817b91fba32a423f486a9e360d1863ae3c17
Dmitry An <dmitry.andreyev@gmail.com>
2023-02-01 12:29:38 +0400

    second commit

---
+++ programming/day02/Program.cs
@@ -0,0 +1,43 @@
+﻿// 1
+// int n = 5;
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+// {
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine($"[{string.Join(", ",array)}]");
+
+
+// 2 multiplication table
+// int m = 5;
+// for (int i = 0; i <m; i++)
+// {
+//     for (int j = 0; j < m; j++)
+//     {
+//         Console.Write($"{(i+1)*(j+1)} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// 3 optimized
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n,n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i,j] = (i+1)*(j+1);
+        matrix[j,i] = (i+1)*(j+1);
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i,j]);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
