e057eafc7533b3366f00128755e2d698e8847929
Dmitry An <dmitry.andreyev@gmail.com>
2023-02-09 16:27:30 +0400

    commit 9 count sort

---
+++ programming/day09/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] array = new int[100];
+Random _rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = _rand.Next(0,10);
+}
+
+int max = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>max)
+    {
+        max = array[i];
+    }
+}
+
+int[] counter = new int[max+1];
+
+for (int i = 0; i < array.Length; i++)
+{
+    counter[array[i]]+=1;
+}
+
+Console.WriteLine($"{string.Join(", ",counter)}");
+
+int[] sortedArray = new int[100];
+
+int position = 0;
+for (int i = 0; i < counter.Length; i++)
+{
+    for (int j = 0; j < counter[i]; j++)
+    {
+        sortedArray[position] = i;
+        position++;
+    }
+}
+
+Console.WriteLine($"{string.Join(", ",sortedArray)}");
